0594382ea684a90d01869d14abcf61ea35d0c282
Joshua Ball <joshua.g.s.ball@gmail.com>
2015-01-02 09:01:03 -0800

    Updating to Full example MVC site (Code Changes and additions)

---
+++ Identity.MVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Identity.MVC5.Startup))]
+namespace Identity.MVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
